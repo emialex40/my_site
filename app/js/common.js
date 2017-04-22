b51252c359a4f25797e8a77d5263414d80088791
emialex40 <26emic73@gmail.com>
2017-04-22 18:44:42 +0300

    22/04/2017

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,21 @@
 $(function() {
 
-	// Custom JS
+$(window).scroll(function() {
+	if($(window).scrollTop() >= 120) {
+		$('.top_menu').animate({
+			opacity: 'hide'
+		}, 100);
+		$('.top_menu_next').animate({
+			height: 'show'
+		}, 1000)
+	}else{
+		$('.top_menu').animate({
+			opacity: 'show'
+		}, 1500);
+		$('.top_menu_next').animate({
+			opacity: 'hide'
+		}, 300)
+	}
+});
 
 });
